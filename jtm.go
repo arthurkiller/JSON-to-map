91ec00b668a1582f83773c342a248fd1afa427bf
lza@arthur <lza@meitu.com>
2016-09-06 15:22:26 +0800

    update the jtm

--- jtm.go
+++ jtm.go
@@ -18,30 +18,8 @@ type jsontomap struct {
 	mapR map[string]string
 }
 
-func (c *jsontomap) Generate(url string) error {
-	c.mapS = make(map[string]interface{}, 1024)
-	c.mapR = make(map[string]string, 1024)
-
-	content := make([]byte, 102400)
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("error in http get: ", err)
-		return err
-	}
-	content, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("io readall error: ", err)
-		return err
-	}
-	err = json.Unmarshal(content, &c.mapS)
-	if err != nil {
-		fmt.Println("format json error : ", err)
-	}
-
-	fmt.Println(c.mapS)
-
-	getMap(c.mapS, c.mapR, "#NULL")
-	return nil
+func Newjtm() JsonToMap {
+	return &jsontomap{}
 }
 
 func getMap(mapS map[string]interface{}, mapR map[string]string, lastkey string) {
@@ -110,4 +88,34 @@ func getMap(mapS map[string]interface{}, mapR map[string]string, lastkey string)
 			}
 		}
 	}
+}
+
+func (c *jsontomap) Generate(url string) error {
+	c.mapS = make(map[string]interface{}, 1024)
+	c.mapR = make(map[string]string, 1024)
+
+	content := make([]byte, 102400)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("error in http get: ", err)
+		return err
+	}
+	content, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("io readall error: ", err)
+		return err
+	}
+	err = json.Unmarshal(content, &c.mapS)
+	if err != nil {
+		fmt.Println("format json error : ", err)
+	}
+
+	fmt.Println(c.mapS)
+
+	getMap(c.mapS, c.mapR, "#NULL")
+	return nil
+}
+
+func (c *jsontomap) Get() map[string]string {
+	return c.mapR
 }
